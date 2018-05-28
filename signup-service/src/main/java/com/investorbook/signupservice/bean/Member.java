e72dafb121155068ca585ee1a6d1aa205441ea6c
Razi Alqasem <Razi.Alqasem@verato.com>
2018-05-28 19:18:52 -0400

    Use DTO pattern

--- signup-service/src/main/java/com/investorbook/signupservice/bean/Member.java
+++ signup-service/src/main/java/com/investorbook/signupservice/bean/Member.java
@@ -4,17 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-@ApiModel(description = "all member details.")
 @Entity
 @Table(name = "members")
 public class Member {
@@ -22,24 +13,12 @@ public class Member {
 	@Email
 	private String email;
 
-	@Size(min = 1, message = "firstname must be at least 1 char long")
-	@ApiModelProperty(notes = "firstname should have at least 1 character")
 	@Column(name = "firstname")
 	private String firstName;
 
-	@Size(min = 1, message = "lastname must be at least 1 char long")
-	@ApiModelProperty(notes = "lastname should have at least 1 character")
 	@Column(name = "lastname")
 	private String lastName;
 
-	// this is not stored, used to receive the password string entered from the UI
-	@Transient
-	@Size(min = 6, message = "password must be at least 6 char long")
-	@NotNull(message = "password cannot be null")
-	private String password;
-
-	// salt+hash, never retrieve it back to the client
-	@JsonIgnore
 	@Column(name = "passwordhash")
 	private byte[] passwordHash;
 
@@ -47,12 +26,11 @@ public class Member {
 		super();
 	}
 
-	public Member(String email, String firstName, String lastName, String password) {
+	public Member(String email, String firstName, String lastName) {
 		super();
 		this.email = email;
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.password = password;
 	}
 
 	public String getEmail() {
@@ -79,14 +57,6 @@ public class Member {
 		this.lastName = lastName;
 	}
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 	public byte[] getPasswordHash() {
 		return passwordHash;
 	}
@@ -94,5 +64,4 @@ public class Member {
 	public void setPasswordHash(byte[] passwordHash) {
 		this.passwordHash = passwordHash;
 	}
-
 }
--- signup-service/src/main/java/com/investorbook/signupservice/service/SignupServiceController.java
+++ signup-service/src/main/java/com/investorbook/signupservice/service/SignupServiceController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.investorbook.common.util.EncryptionUtil;
 import com.investorbook.signupservice.bean.Member;
 import com.investorbook.signupservice.dao.MemberRepository;
+import com.investorbook.signupservice.dto.MemberDto;
 import com.investorbook.signupservice.exception.MemberAlreadyExistsException;
 import com.investorbook.signupservice.exception.MemberNotFoundException;
 
@@ -24,37 +26,49 @@ public class SignupServiceController {
 	@Autowired
 	private MemberRepository memberRepository;
 
+	
+	
 	@PostMapping("/signup")
-	public ResponseEntity<Member> signUpMember(@Valid @RequestBody Member member) {
+	public ResponseEntity<MemberDto> signUpMember(@Valid @RequestBody MemberDto memberDto) {
 		// member already exists
-		if (memberRepository.existsById(member.getEmail())) {
+		if (memberRepository.existsById(memberDto.getEmail())) {
 			throw new MemberAlreadyExistsException("email already exists");
 		}
+		
+		ModelMapper modelMapper = new ModelMapper();
 
-		member.setPasswordHash(EncryptionUtil.encode(EncryptionUtil.hash(member.getPassword())));
+		Member member = modelMapper.map(memberDto, Member.class);
+		
+		member.setPasswordHash(EncryptionUtil.encode(EncryptionUtil.hash(memberDto.getPassword())));
+		
+		member = memberRepository.save(member);
+		MemberDto result = modelMapper.map(member, MemberDto.class);
+		return ResponseEntity.ok(result);
+		
 		
 		//TODO call email service to send welcome message,this can be implemented using messaging to create the email asynchronously 
-		return ResponseEntity.ok(memberRepository.save(member));
 	}
 
 	@PostMapping("/signin")
-	public ResponseEntity<Member> signInMember(@Valid @RequestBody Member member) {
+	public ResponseEntity<MemberDto> signInMember(@Valid @RequestBody MemberDto memberDto) {
 		// user not found
 
-		Optional<Member> existingMember = memberRepository.findById(member.getEmail());
+		Optional<Member> existingMember = memberRepository.findById(memberDto.getEmail());
 		if (!existingMember.isPresent()) {
-			throw new MemberNotFoundException(member.getEmail() + " is not found please sign up");
+			throw new MemberNotFoundException(memberDto.getEmail() + " is not found please sign up");
 		}
 		// hash the password to match with existing password
-		byte[] password = EncryptionUtil.hashToMatch(member.getPassword(),
+		byte[] password = EncryptionUtil.hashToMatch(memberDto.getPassword(),
 				EncryptionUtil.decode(existingMember.get().getPasswordHash()));
 
+		ModelMapper modelMapper = new ModelMapper();
+		
 		if (Arrays.equals(password, EncryptionUtil.decode(existingMember.get().getPasswordHash()))) {
 			// TODO create token;
-			return ResponseEntity.ok(existingMember.get());
+			return ResponseEntity.ok(modelMapper.map(existingMember.get(), MemberDto.class));
 		}
 
 		//not logged in
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(existingMember.get());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(modelMapper.map(existingMember.get(), MemberDto.class));
 	}
 }
